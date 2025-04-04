c233b7c81477fe7c4f2b2cbb629fad0171d90cf4
AshrafJB2 <achrafjabbari1@gmail.com>
2025-04-04 01:07:38 +0000

    add movie search functionality and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,54 @@
+import './App.css'
+import SearchIcon from './assets/search.svg'
+import {useEffect, useState} from "react";
+import MovieCard from "./moviecard";
+
+
+const APIURl = `http://www.omdbapi.com/?i=tt3896198&apikey=c8b48bf`
 
 export default function App() {
 
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    async function movieSearch(title) {
+        const response = await fetch(`${APIURl}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+    useEffect(() =>{movieSearch('spiderman')},[]);
+
+    return(
+        <div className="app">
+            <h1>Movies</h1>
+            <div className="search">
+                <input
+                    placeholder="search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                <img 
+                src={SearchIcon}
+                alt="search"
+                onClick={() => movieSearch(searchTerm)}/>
+
+            </div>
+            <div className="container">
+                {
+                    movies?.length > 0 ? (
+                        movies.map((movie) => <MovieCard movie={movie} key={movie.imdbID}/>)
+                    ) : (
+                        <div className="empty">
+                            <h2>No Movies Found</h2>
+                        </div>
+                    )
+                }
+            </div> 
+
+
+
+
+
+        </ div>
+    )
 }
---
+++ src/moviecard.jsx
@@ -0,0 +1,13 @@
+export default function MovieCard({movie}) {
+    return (
+        <div className="movie">
+                    <div><p>{movie.Year}</p></div>
+                    <div><img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://placehold.co/400'} alt={movie.title} /></div>
+                    <div>
+                    <span>{movie.Type}</span>
+                    <h3>{movie.Title}</h3>
+                    </div>
+                </div>
+
+    )
+}
